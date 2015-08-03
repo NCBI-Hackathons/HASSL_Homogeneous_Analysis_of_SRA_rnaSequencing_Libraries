0f451458b31aa6d909f8228d4b0452572a712d12
Russell Durrett <russdurrett@gmail.com>
2015-08-03 13:39:07 -0400

    fixed runtime

--- coralsnake.py
+++ coralsnake.py
@@ -2,7 +2,7 @@
 
 REF = 'GRCh38.p4'
 
-HISATREF = "/home/ubuntu/russ/ncbi/hisat_index/GRCh38.p4"
+HISATREF = "/home/ubuntu/refs/hisat_index/GRCh38.p4"
 # STAR50REF = 
 # STAR100REF = 
 
@@ -11,22 +11,32 @@ HISATREF = "/home/ubuntu/russ/ncbi/hisat_index/GRCh38.p4"
 THREADS = 8 
 
 
-# rule cufflink_count
-# 	output: counts
-# 	input: gff3 + sorted.bam 
-
-sample = "SRR1295542" 
 
 rule all: 
 	input: "SRR1295542.GRCh38.p4.hisat.sorted.bam"
 
 
 
-# rule index_sam:
-# 	output: "{sample}.{REF}.{alner}.sorted.sam.sai"
-# 	input: "{sample}.{ref}.{alner}.sorted.sam"
-# 	message: "indexing sam {input}"
-# 	shell: "{samtools sortsam? }
+# SRA -> PILEUP -> RAW COUNTS OFF NCBI GFF3 
+
+
+rule kar_sra: 
+	output: "{sample}.{REF}.{alner}.sorted.sra"
+	input: "{sample}.{REF}.{alner}.sorted.sradir"
+	message: "converting sradir {input} to sra archive file {output}"
+	shell: "kar -c {output} -d {input}"
+
+rule sort_sra:
+	output: "{sample}.{REF}.{alner}.sorted.sradir"
+	input: "{sample}.{REF}.{alner}.sradir"
+	message: "sorting sradir {input} to {output}" 
+	shell: "sra-sort {input} {output}"
+
+rule bam_to_sra:
+	output: "{sample}.{REF}.{alner}.sradir"
+	input: "{sample}.{REF}.{alner}.bam"
+	message: "converting {input} bam to sra {output}"
+	shell: "bam-load -o {sample}.{REF}.{alner}.sradir -k <config-GI-file> {input}"
 
 rule sort_bam:
 	output: "{sample}.{REF}.{alner}.sorted.bam"
@@ -43,7 +53,7 @@ rule sam_to_bam:
 rule hisat_alignment:
 	output: "{sample}.{REF}.hisat.sam"
 	# input: "{sample}.fastq"
-	message: "hisat aligning reads {sample}.fastq to {REF} to produce {output}"
+	message: "hisat aligning reads {sample}.fastq to {REF} with {THREADS} threads to produce {output}"
 	shell: "hisat -x {HISATREF} -p {THREADS} --sra-acc {sample} -S {sample}.{REF}.hisat.sam"
 
 # rule star_alignment: 
