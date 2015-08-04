f3c549b2637d91851faadba2666765cb89d6c1b7
Russell Durrett <russdurrett@gmail.com>
2015-08-03 21:23:01 -0400

    logs transferring as well

--- bamsnake.py
+++ bamsnake.py
@@ -48,16 +48,16 @@ rule sam_to_bam:
 
 
 rule hisat_alignment_two:
-	output: "{sample}.GRCh38.p4.hisat.sam", "{sample}.hisat.2.log"
+	output: "{sample}.GRCh38.p4.hisat.sam", "{sample}.hisat.log"
 	input: "{sample}.hisat.novel.splicesite.txt"
 	message: "running second pass alignment"
 	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {THREADS} --sra-acc {sample} --mm -t -S {sample}.GRCh38.p4.hisat.sam --novel-splicesite-infile {sample}.hisat.novel.splicesite.txt 2>> {sample}.hisat.log"
 
 
 rule hisat_alignment_one: 
-	output: "{sample}.hisat.novel.splicesite.txt", "{sample}.hisat.log"
+	output: "{sample}.hisat.novel.splicesite.txt"
 	message: "hisat aligning reads from {sample} to GRCh38.p4 with {THREADS} to produce splicesites"
-	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {THREADS} --sra-acc {sample} --mm -t -S {sample}.GRCh38.p4.firstpass.sam --novel-splicesite-outfile {sample}.hisat.novel.splicesite.txt 2> {sample}.hisat.log"
+	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {THREADS} --sra-acc {sample} --mm -t -S {sample}.GRCh38.p4.firstpass.sam --novel-splicesite-outfile {output} 2> {sample}.hisat.log"
 
 
 
