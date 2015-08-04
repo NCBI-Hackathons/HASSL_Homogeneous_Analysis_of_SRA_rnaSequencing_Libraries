2174063c329ebe18d39ad4ac35e61db24bb85335
Russell Durrett <russdurrett@gmail.com>
2015-08-03 22:15:31 -0400

    logs transferring as well

--- bamsnake.py
+++ bamsnake.py
@@ -19,7 +19,7 @@ rule transfer_logs_s3:
 	output: touch("{sample}.transferred.log")
 	input:  "{sample}.hisat.log"
 	message: "transferring {input}'s logs to S3"
-	shell: "s3cmd put {sample}.hisat.1.log s3://ncbi-hackathon-aug/rnamapping/"
+	shell: "s3cmd put {sample}.hisat.one.log s3://ncbi-hackathon-aug/rnamapping/ ; s3cmd put {sample}.hisat.two.log s3://ncbi-hackathon-aug/rnamapping/"
 
 rule transfer_bam_s3: 
 	output: touch("{sample}.transferred")
@@ -48,18 +48,17 @@ rule sam_to_bam:
 
 
 rule hisat_alignment_two:
-	output: "{sample}.GRCh38.p4.hisat.sam", "{sample}.hisat.log"
+	output: "{sample}.GRCh38.p4.hisat.sam", "{sample}.hisat.two.log"
 	input: "{sample}.hisat.novel.splicesites.txt"
 	message: "running second pass alignment"
-	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {THREADS} --sra-acc {sample} --mm -t -S {sample}.GRCh38.p4.hisat.sam --novel-splicesite-infile {sample}.hisat.novel.splicesite.txt 2>> {sample}.hisat.log"
+	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {THREADS} --sra-acc {sample} --mm -t -S {sample}.GRCh38.p4.hisat.sam --novel-splicesite-infile {sample}.hisat.novel.splicesite.txt 2> {sample}.hisat.two.log"
 
 
 rule hisat_alignment_one: 
-	output: "{sample}.hisat.novel.splicesites.txt"
-	input: "tmp"
+	output: "{sample}.hisat.novel.splicesites.txt", "{sample}.hisat.one.log"
 	message: "hisat aligning reads from {sample} to GRCh38.p4 with {THREADS} to produce splicesites"
 	threads: 10 
-	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {threads} --sra-acc {sample} --mm -t -S {sample}.GRCh38.p4.firstpass.sam --novel-splicesite-outfile {output} 2> {sample}.hisat.log"
+	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {threads} --sra-acc {sample} --mm -t -S {sample}.GRCh38.p4.firstpass.sam --novel-splicesite-outfile {output} 2> {sample}.hisat.first.log"
 
 
 
