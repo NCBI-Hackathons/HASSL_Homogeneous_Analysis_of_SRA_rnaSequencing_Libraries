516f41e5365c0a6e3efed6f2841f12436d83d9e6
Russell Durrett <russdurrett@gmail.com>
2015-08-04 10:06:57 -0400

    this bug will be the end of me

--- lightsnake.py
+++ lightsnake.py
@@ -10,7 +10,7 @@ HISATREF = "/home/ubuntu/refs/hisat_index/GRCh38.p4"
 SAMPLES = 'SRR2089122'.split()
 
 rule all: 
-	input: expand("{sample}.hisat.novel.splicesites.txt", sample=SAMPLES), expand("{sample}.GRCh38.p4.hisat.sorted.bam", sample=SAMPLES)
+	input: expand("{sample}.GRCh38.p4.hisat.novel.splicesites.txt", sample=SAMPLES), expand("{sample}.GRCh38.p4.hisat.sorted.bam", sample=SAMPLES)
 
 
 # SRA -> PILEUP -> RAW COUNTS OFF NCBI GFF3 
@@ -30,7 +30,7 @@ rule sam_to_bam:
 
 
 rule hisat_alignment_two:
-	output: "{sample}.GRCh38.p4.hisat.sam", "{sample}.hisat.two.log"
+	output: "{sample}.GRCh38.p4.hisat.sam", "{sample}.GRCh38.p4.hisat.two.log"
 	input: "{sample}.hisat.novel.splicesites.txt"
 	threads: 10
 	message: "running second pass hisat alignment with {threads} threads"
@@ -38,7 +38,7 @@ rule hisat_alignment_two:
 
 
 rule hisat_alignment_one: 
-	output: "{sample}.hisat.novel.splicesites.txt", "{sample}.hisat.one.log"
+	output: "{sample}.GRCh38.p4.hisat.novel.splicesites.txt", "{sample}.GRCh38.p4.hisat.one.log"
 	threads: 10 
 	message: "hisat aligning reads from {sample} to GRCh38.p4 with {threads} threads to produce splicesites"
 	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {threads} --sra-acc {sample} --mm -t --novel-splicesite-outfile {sample}.hisat.novel.splicesites.txt > /dev/null  2> {sample}.hisat.one.log"
