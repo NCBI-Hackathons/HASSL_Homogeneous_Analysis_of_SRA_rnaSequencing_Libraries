65627bbc73eb9435907d8dfff20f809ad90d0103
Russell Durrett <russdurrett@gmail.com>
2015-08-04 10:10:50 -0400

    this bug will be the end of me

--- lightsnake.py
+++ lightsnake.py
@@ -41,7 +41,7 @@ rule hisat_alignment_one:
 	output: "{sample}.GRCh38.p4.hisat.novel.splicesites.txt", "{sample}.GRCh38.p4.hisat.one.log"
 	threads: 10 
 	message: "hisat aligning reads from {sample} to GRCh38.p4 with {threads} threads to produce splicesites"
-	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {threads} --sra-acc {sample} --mm -t --novel-splicesite-outfile {sample}.GRCh38.p4.hisat.novel.splicesites.txt > /dev/null  2> {sample}.GRCh38.p4.hisat.one.log"
+	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {threads} --sra-acc {wildcards.sample} --mm -t --novel-splicesite-outfile {wildcards.sample}.GRCh38.p4.hisat.novel.splicesites.txt > /dev/null  2> {sample}.GRCh38.p4.hisat.one.log"
 
 
 
