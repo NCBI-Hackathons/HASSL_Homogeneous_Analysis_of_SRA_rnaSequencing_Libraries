a55308dc7fbc12aa1855313184509027de40aa8d
Russell Durrett <russdurrett@gmail.com>
2015-08-03 21:26:44 -0400

    logs transferring as well

--- bamsnake.py
+++ bamsnake.py
@@ -29,7 +29,7 @@ rule transfer_bam_s3:
 
 rule transfer_splices_s3:
 	output: touch("{sample}.transferred.splices")
-	input: "{sample}.hisat.novel.splicesite.txt"
+	input: "{sample}.hisat.novel.splicesites.txt"
 	message: "transferring hisat splice sites {input} to s3"
 	shell: "s3cmd put {input} s3://ncbi-hackathon-aug/rnamapping/"	
 
@@ -49,13 +49,13 @@ rule sam_to_bam:
 
 rule hisat_alignment_two:
 	output: "{sample}.GRCh38.p4.hisat.sam", "{sample}.hisat.log"
-	input: "{sample}.hisat.novel.splicesite.txt"
+	input: "{sample}.hisat.novel.splicesites.txt"
 	message: "running second pass alignment"
 	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {THREADS} --sra-acc {sample} --mm -t -S {sample}.GRCh38.p4.hisat.sam --novel-splicesite-infile {sample}.hisat.novel.splicesite.txt 2>> {sample}.hisat.log"
 
 
 rule hisat_alignment_one: 
-	output: "{sample}.hisat.novel.splicesite.txt"
+	output: "{sample}.hisat.novel.splicesites.txt"
 	message: "hisat aligning reads from {sample} to GRCh38.p4 with {THREADS} to produce splicesites"
 	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {THREADS} --sra-acc {sample} --mm -t -S {sample}.GRCh38.p4.firstpass.sam --novel-splicesite-outfile {output} 2> {sample}.hisat.log"
 
