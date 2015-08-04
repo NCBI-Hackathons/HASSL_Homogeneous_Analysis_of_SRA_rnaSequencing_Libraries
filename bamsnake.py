361ae39a69054a9af8a5382aa8bbc92f2e94c3e7
Russell Durrett <russdurrett@gmail.com>
2015-08-03 23:10:20 -0400

    logs transferring as well

--- bamsnake.py
+++ bamsnake.py
@@ -57,9 +57,9 @@ rule hisat_alignment_two:
 
 rule hisat_alignment_one: 
 	output: "{sample}.hisat.novel.splicesites.txt", "{sample}.hisat.one.log"
-	message: "hisat aligning reads from {sample} to GRCh38.p4 with {THREADS} to produce splicesites"
 	threads: 10 
-	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {threads} --sra-acc {sample} --mm -t -S {sample}.GRCh38.p4.firstpass.sam --novel-splicesite-outfile {output} 2> {sample}.hisat.first.log"
+	message: "hisat aligning reads from {sample} to GRCh38.p4 with {threads} to produce splicesites"
+	shell: "hisat -D 15 -R 2 -N 0 -L 22 -i S,1,1.15 -x {HISATREF} -p {threads} --sra-acc {sample} --mm -t --novel-splicesite-outfile {output} > /dev/null  2> {sample}.hisat.one.log"
 
 
 
