c0e9951a05a0d70d992ae09fac7ae3f8a13dcdea
Russell Durrett <russdurrett@gmail.com>
2015-08-27 22:54:29 -0500

    reworked reference paths

--- hassl.py
+++ hassl.py
@@ -5,19 +5,19 @@
 import os 
 
 #set the number of threads to use in alignments and sorting
-THREADS=9
+THREADS=3
 
 REFERENCE_BASE_URL="https://s3.amazonaws.com/genomicdata/HASSL"
 REFERENCE_DIR="/mnt/resources"
 HISAT_REFERENCE_DIR = REFERENCE_DIR + "/hisat_indexes"
-HISATREF_BASENAME = "Homo_sapiens.GRCh38.dna.toplevel"
+HISATREF_BASENAME = "Homo_sapiens.GRCh38.dna_rm.toplevel"
+FASTA_URL="ftp://ftp.ensembl.org/pub/release-81/fasta/homo_sapiens/dna/" + HISATREF_BASENAME + ".fa.gz"
 #HISATREF_BASENAME = "GRCh38.p4"
 PICARDFLATFILE_NAME = "GRCh38.77.compatible.ucsc.picard.refflat.txt"
 GTFFILE_NAME = "Ensembl.GRCh38.77.gtf"
 SPLICEFILE_NAME = "Ensembl.GRCh38.77.splicesites.txt"
 
-#HISATREF=HISAT_REFERENCE_DIR + "/" + HISATREF_BASENAME
-HISATREF=REFERENCE_DIR + "/" + HISATREF_BASENAME
+HISATREF=HISAT_REFERENCE_DIR + "/" + HISATREF_BASENAME
 PICARDFLATFILE=REFERENCE_DIR+ "/" + PICARDFLATFILE_NAME
 GTFFILE=REFERENCE_DIR+ "/" + GTFFILE_NAME
 SPLICEFILE=REFERENCE_DIR+ "/" + SPLICEFILE_NAME
@@ -68,7 +68,7 @@ rule project_counts:
   output: "project.featureCounts"
   input: GTFFILE, expand("bams/{sample}.GRCh38.ens77.hisat.sorted.bam", sample=SAMPLES), GTFFILE
   log: "log/project.featureCounting.log"
-  threads: 30 
+  threads: THREADS
   message: "performing overall project feature counting"
   shell: "{FEATURECOUNTS} -T {threads} --primary -F GTF -t exon -g gene_id -a {GTFFILE} -o project.featureCounts bams/*.bam 2>&1 > log/project.featureCounting.log"
 
@@ -128,31 +128,24 @@ rule hisat_alignment:
 #SCRIPT TO GATHER REFERENCES AND ANNOTATION FILES FOR COBRASNAKE RNASEQ PIPELINE
 
 rule resources:
-  input:  [PICARDFLATFILE, GTFFILE, SPLICEFILE, "/mnt/hassl/lib/GRCh38.p4.rev.6.bt2"]
+  input:  [PICARDFLATFILE, GTFFILE, SPLICEFILE, HISAT_REFERENCE_DIR+"/"+HISATREF_BASENAME+".rev.2.bt2l"]
 
-    # "{REFERENCE_DIR}/GCF_000001405.30_GRCh38.p4_genomic.gff",
-    # "{REFERENCE_DIR}/GCF_000001405.30_GRCh38.p4_genomic.fna",
-    # "{REFERENCE_DIR}/GRCh38.p4.1.bt2", "{REFERENCE_DIR}/GRCh38.p4.3.bt2", "{REFERENCE_DIR}/GRCh38.p4.5.bt2", "{REFERENCE_DIR}/GRCh38.p4.rev.1.bt2", "{REFERENCE_DIR}/GRCh38.p4.rev.5.bt2",
-    #       "{REFERENCE_DIR}/GRCh38.p4.2.bt2", "{REFERENCE_DIR}/GRCh38.p4.4.bt2", "{REFERENCE_DIR}/GRCh38.p4.6.bt2", "{REFERENCE_DIR}/GRCh38.p4.rev.2.bt2", "{REFERENCE_DIR}/GRCh38.p4.rev.6.bt2"
-
-# Reference genome (h38)
 rule hasat_index:
-  output: "{REFERENCE_DIR}/GRCh38.p4.1.bt2", "{REFERENCE_DIR}/GRCh38.p4.3.bt2", "{REFERENCE_DIR}/GRCh38.p4.5.bt2", "{REFERENCE_DIR}/GRCh38.p4.rev.1.bt2", "{REFERENCE_DIR}/GRCh38.p4.rev.5.bt2",
-          "{REFERENCE_DIR}/GRCh38.p4.2.bt2", "{REFERENCE_DIR}/GRCh38.p4.4.bt2", "{REFERENCE_DIR}/GRCh38.p4.6.bt2", "{REFERENCE_DIR}/GRCh38.p4.rev.2.bt2", "{REFERENCE_DIR}/GRCh38.p4.rev.6.bt2"
-  input: "{REFERENCE_DIR}/GCF_000001405.30_GRCh38.p4_genomic.fna"
-  message: "indexing human genome"
-  shell: "{HISAT_BUILD} {input} {REFERENCE_DIR}/{HISATREF_BASENAME}"
+  output: HISAT_REFERENCE_DIR + "/" + HISATREF_BASENAME + ".rev.2.bt2l"
+  input: HISAT_REFERENCE_DIR + "/" + HISATREF_BASENAME + ".fa"
+  message: "hisat-build indexing human genome {input}"
+  shell: "{HISAT_BUILD} {input} {HISAT_REFERENCE_DIR}/{HISATREF_BASENAME}"
 
 rule gunzipHumRef:
-  output: "{REFERENCE_DIR}/GCF_000001405.30_GRCh38.p4_genomic.fna" 
-  input: "{REFERENCE_DIR}/GCF_000001405.30_GRCh38.p4_genomic.fna.gz"
+  output: REFERENCE_DIR + "/" + HISATREF_BASENAME + ".fa"
+  input: REFERENCE_DIR + "/" + HISATREF_BASENAME + ".fa.gz"
   message: "extracting human genome fasta.gz"
   shell: "gunzip -c {input} > {output}"
   
 rule get_humanreference:
-  output: temp("{REFERENCE_DIR}/GCF_000001405.30_GRCh38.p4_genomic.fna.gz")
+  output: temp(REFERENCE_DIR + "/" + HISATREF_BASENAME + ".fa.gz")
   message: "downloading human reference genome"
-  shell: "wget -P {REFERENCE_DIR} ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF_000001405.30_GRCh38.p4/GCF_000001405.30_GRCh38.p4_genomic.fna.gz"
+  shell: "wget -P {REFERENCE_DIR} {FASTA_URL}"
 
 
 rule get_splicesites:
